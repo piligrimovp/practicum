398d7896da8581772cdab78857a77385bccd8cf7
p.piligrimov <p.piligrimov@iq-dev.digital>
2020-03-17 23:51:04 +0500

    Что сделал: дописаны api для чисел Фибоначчи, названия регионов. Переделана валидация входных данных. Что буду делать: 2 лаб. работу. Вопросы: нет

--- lab1/lab1/Controllers/ValuesController.cs
+++ lab1/lab1/Controllers/ValuesController.cs
@@ -9,14 +9,18 @@ namespace lab1.Controllers
 {
     [ApiController]
     public class ValuesController : ControllerBase
-    {
+    { 
+
         [Route("/")]
-        public string Get()
+        public string Get() 
         {
             string[] map =
             {
                 "Число прописью - /ntw/123",
                 "Решение квадратного уравнения - /equation?a=1&b=0&c=0",
+                "День недели заданной даты - /day-week?date=17.03.2020",
+                "Число Фибоначчи - /fibonacci/1",
+                "Название региона - /region/72",
             };
             return JsonConvert.SerializeObject(map);
         }
@@ -42,27 +46,9 @@ namespace lab1.Controllers
         /// <param name="c">Свободный коэффициент</param>
         /// <returns>Корни квадратного уравнения</returns>
         [Route("equation")]
-        public string GetQuadraticEquation(string a, string b, string c)
+        public string GetQuadraticEquation(double a, double b, double c = 0)
         {
-            string answerFail = "Параметры не являются числами";
-            double _a, _b, _c;
-            if (double.TryParse(a, out _a)) {}
-            else
-            {
-                return JsonConvert.SerializeObject(answerFail);
-            }
-            if (double.TryParse(b, out _b)) {}
-            else
-            {
-                return JsonConvert.SerializeObject(answerFail);
-            }
-            if (double.TryParse(c, out _c)) {}
-            else
-            {
-                _c = 0;
-            }
-
-            var Roots = QuadraticEquation.Roots(_a, _b, _c);
+            var Roots = QuadraticEquation.Roots(a, b, c);
             string answer = "";
             if(Roots == null)
             {
@@ -72,6 +58,75 @@ namespace lab1.Controllers
                 answer = JsonConvert.SerializeObject(Roots);
             }
             return answer;
+        }
+
+        /// <summary>
+        /// Возвращает день недели по заданной дате
+        /// </summary>
+        /// <param name="date">дата в формате день.месяц.год</param>
+        /// <returns></returns>
+        [Route("day-week")]
+        public ActionResult<string> GetDayOfWeek(string date)
+        {
+            string[] days =
+            {
+                "Воскресенье",
+                "Понедельник",
+                "Вторник",
+                "Среда",
+                "Четверг",
+                "Пятница",
+                "Суббота",
+            };
+            if(DateTime.TryParse(date, out DateTime dt))
+            {
+                return JsonConvert.SerializeObject(days[(int)dt.DayOfWeek]);
+            }
+            return JsonConvert.SerializeObject("Некорректная дата");
+            
+        }
+
+        /// <summary>
+        /// Возвращает число Фибоначчи
+        /// </summary>
+        /// <param name="index">порядковый номер числа</param>
+        /// <returns></returns>
+        [Route("fibonacci/{index}")]
+        public string GetFibonacci(int index)
+        {
+            bool neg = false;
+            if(index < 0)
+            {
+                neg = true;
+                index = -index;
+            }
+           
+            double a = Math.Pow((1 + Math.Sqrt(5)) / 2, index);
+            double b = Math.Pow((1 - Math.Sqrt(5)) / 2, index);
+
+            double answer = (a - b) / Math.Sqrt(5);
+
+            if(neg && index % 2 == 0)
+            {
+                answer = -answer;
+            }
+
+            return JsonConvert.SerializeObject(Math.Round(answer));
+        }
+
+        /// <summary>
+        /// Возвращает название региона
+        /// </summary>
+        /// <param name="number">Код региона</param>
+        /// <returns></returns>
+        [Route("region/{number}")]
+        public ActionResult<string> GetNameRegion(int number)
+        {
+            if(number < 0 || number >= Regions.regions.Length || Regions.regions[number] == null)
+            {
+                return BadRequest(JsonConvert.SerializeObject("Регион не найден"));
+            }
+            return Content(JsonConvert.SerializeObject(Regions.regions[number]));
         }
 
     }
